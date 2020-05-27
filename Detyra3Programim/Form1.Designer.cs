aa4f0e264e69ae17c1693889d198f6ea824ae49d
Bekim Peci <bekkim.peci@gmail.com>
2020-05-27 22:27:31 +0200

    first commit

---
+++ Detyra3Programim/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Detyra3Programim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFile = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblEditor = new System.Windows.Forms.Label();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.Search = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.Search.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(12, 22);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(26, 13);
+            this.lblFile.TabIndex = 0;
+            this.lblFile.Text = "File:";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(44, 22);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(532, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(582, 22);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // lblEditor
+            // 
+            this.lblEditor.AutoSize = true;
+            this.lblEditor.Location = new System.Drawing.Point(41, 74);
+            this.lblEditor.Name = "lblEditor";
+            this.lblEditor.Size = new System.Drawing.Size(58, 13);
+            this.lblEditor.TabIndex = 3;
+            this.lblEditor.Text = "Text Editor";
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Location = new System.Drawing.Point(36, 90);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEditor.Size = new System.Drawing.Size(540, 380);
+            this.txtEditor.TabIndex = 4;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(6, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(153, 20);
+            this.txtSearch.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(165, 17);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(0, 19);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(153, 20);
+            this.txtCount.TabIndex = 9;
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(159, 16);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 10;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(1, 60);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(90, 13);
+            this.lblResult.TabIndex = 11;
+            this.lblResult.Text = "Results of count: ";
+            // 
+            // Search
+            // 
+            this.Search.Controls.Add(this.txtSearch);
+            this.Search.Controls.Add(this.btnSearch);
+            this.Search.Location = new System.Drawing.Point(626, 74);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(242, 78);
+            this.Search.TabIndex = 12;
+            this.Search.TabStop = false;
+            this.Search.Text = "Search";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCount);
+            this.groupBox1.Controls.Add(this.btnCount);
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Location = new System.Drawing.Point(626, 167);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 83);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Count";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(630, 272);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(238, 154);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Additional GUI";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(692, 482);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(793, 482);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 543);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.lblEditor);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.lblFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Search.ResumeLayout(false);
+            this.Search.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblEditor;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.GroupBox Search;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
